f8e06ca2d184d6b93b77847a6dc5b140dd24de9e
adhe223 <adhe223@g.uky.edu>
2019-05-29 09:22:11 -0700

    Build example budget table

--- src/components/BudgetTable/BudgetTable.tsx
+++ src/components/BudgetTable/BudgetTable.tsx
@@ -1,11 +1,11 @@
-import Paper from "@material-ui/core/Paper";
-import Table from "@material-ui/core/Table";
-import TableBody from "@material-ui/core/TableBody";
-import TableCell from "@material-ui/core/TableCell";
-import TableHead from "@material-ui/core/TableHead";
-import TableRow from "@material-ui/core/TableRow";
-import React, { SFC } from "react";
-import { Transaction } from "../../data/types";
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import React, { SFC } from 'react';
+import { Transaction } from '../../data/types';
 
 export interface BudgetTableProps {
   transactions: Transaction[];
@@ -13,33 +13,26 @@ export interface BudgetTableProps {
 
 const BudgetTable: SFC<BudgetTableProps> = (props: BudgetTableProps) => {
   return (
-    <Paper className={classes.root}>
-      <Table className={classes.table}>
+    <Paper>
+      <Table>
         <TableHead>
           <TableRow>
-            <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell align="right">Calories</TableCell>
-            <TableCell align="right">Fat (g)</TableCell>
-            <TableCell align="right">Carbs (g)</TableCell>
-            <TableCell align="right">Protein (g)</TableCell>
+            <TableCell>Amount</TableCell>
+            <TableCell>User Name</TableCell>
           </TableRow>
         </TableHead>
-        <TableBody>
-          {rows.map(row => (
-            <TableRow key={row.id}>
-              <TableCell component="th" scope="row">
-                {row.name}
-              </TableCell>
-              <TableCell align="right">{row.calories}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
-              <TableCell align="right">{row.carbs}</TableCell>
-              <TableCell align="right">{row.protein}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
+        <TableBody>{getBudgetTableRows(props.transactions)}</TableBody>
       </Table>
     </Paper>
   );
 };
+
+const getBudgetTableRows = (transactions: Transaction[]) =>
+  transactions.map(transaction => (
+    <TableRow key={transaction.id}>
+      <TableCell>{transaction.amount}</TableCell>
+      <TableCell>{transaction.transactor.name}</TableCell>
+    </TableRow>
+  ));
 
 export default BudgetTable;
