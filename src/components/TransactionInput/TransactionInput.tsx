5edc4abc9410d1163910aca2ecbef0d2b82beae7
adhe223 <adhe223@g.uky.edu>
2019-05-30 20:54:25 -0700

    Add input for category and note

--- src/components/TransactionInput/TransactionInput.tsx
+++ src/components/TransactionInput/TransactionInput.tsx
@@ -2,6 +2,7 @@ import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
 import Input from '@material-ui/core/Input';
 import InputAdornment from '@material-ui/core/InputAdornment';
+import TextField from '@material-ui/core/TextField';
 import React, { ChangeEvent, FC, FormEvent, useState } from 'react';
 import { Transaction, User } from '../../data/types';
 
@@ -15,9 +16,19 @@ export interface TransactionInputState {
 
 const TransactionInput: FC<TransactionInputProps> = ({ onSubmit }) => {
   const [amount, setAmount] = useState('');
+  const [category, setCategory] = useState('');
+  const [note, setNote] = useState('');
 
   const handleAmountChange = (event: ChangeEvent<HTMLInputElement>) => {
     setAmount(event.currentTarget.value);
+  };
+
+  const handleCategoryChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setCategory(event.currentTarget.value);
+  };
+
+  const handleNoteChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setNote(event.currentTarget.value);
   };
 
   const handleSubmit = (event: FormEvent<HTMLElement>) => {
@@ -38,6 +49,12 @@ const TransactionInput: FC<TransactionInputProps> = ({ onSubmit }) => {
           onChange={handleAmountChange}
           startAdornment={<InputAdornment position="start">$</InputAdornment>}
         />
+      </Grid>
+      <Grid item={true}>
+        <TextField label="Category" value={category} onChange={handleCategoryChange} />
+      </Grid>
+      <Grid item={true}>
+        <TextField label="Note" value={note} onChange={handleNoteChange} />
       </Grid>
       <Grid item={true}>
         <Button
