430303ffaaf3dba7aa640ce64aef11759d6535ff
adhe223 <adhe223@g.uky.edu>
2019-05-30 17:59:57 -0700

    Spruce up the input. Add tslint. Update material ui

--- src/components/TransactionInput/TransactionInput.tsx
+++ src/components/TransactionInput/TransactionInput.tsx
@@ -1,5 +1,7 @@
 import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
+import Grid from '@material-ui/core/Grid';
+import Input from '@material-ui/core/Input';
+import InputAdornment from '@material-ui/core/InputAdornment';
 import React, { ChangeEvent, FC, FormEvent, useState } from 'react';
 import { Transaction, User } from '../../data/types';
 
@@ -8,19 +10,20 @@ export interface TransactionInputProps {
 }
 
 export interface TransactionInputState {
-  amount: number;
+  amount: string;
 }
 
 const TransactionInput: FC<TransactionInputProps> = ({ onSubmit }) => {
-  const [amount, setAmount] = useState(0);
+  const [amount, setAmount] = useState('');
 
   const handleAmountChange = (event: ChangeEvent<HTMLInputElement>) => {
-    const newValue = parseInt(event.currentTarget.value, 10);
-    setAmount(newValue);
+    setAmount(event.currentTarget.value);
   };
 
   const handleSubmit = (event: FormEvent<HTMLElement>) => {
-    const submitAmount = isNaN(amount) ? 0 : amount;
+    const parsedAmount = parseFloat(amount);
+    const submitAmount = isNaN(parsedAmount) ? 0 : parsedAmount;
+
     onSubmit({
       amount: submitAmount,
       transactor: {} as User,
@@ -28,12 +31,26 @@ const TransactionInput: FC<TransactionInputProps> = ({ onSubmit }) => {
   };
 
   return (
-    <form>
-      <TextField label="Amount" variant="outlined" type="number" value={amount} onChange={handleAmountChange} />
-      <Button variant="contained" color="primary" onSubmit={handleSubmit}>
-        Spend
-      </Button>
-    </form>
+    <Grid container={true} spacing={2} justify="center">
+      <Grid item={true}>
+        <Input
+          value={amount}
+          onChange={handleAmountChange}
+          startAdornment={<InputAdornment position="start">$</InputAdornment>}
+        />
+      </Grid>
+      <Grid item={true}>
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={event => {
+            handleSubmit(event);
+          }}
+        >
+          Spend
+        </Button>
+      </Grid>
+    </Grid>
   );
 };
 
