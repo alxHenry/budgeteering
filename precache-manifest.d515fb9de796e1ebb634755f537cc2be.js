92d9ac9db193c7e577d6f0f9b430d00ae84a7b11
Alex Henry <adhe223@g.uky.edu>
2019-06-09 10:58:24 -0700

    Updates

---
+++ precache-manifest.d515fb9de796e1ebb634755f537cc2be.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b92edf78e07432a6f2627aeedc7f3bb",
+    "url": "/budgeteering/index.html"
+  },
+  {
+    "revision": "a524c3e4483f4cd5a23e",
+    "url": "/budgeteering/static/css/main.37ac8543.chunk.css"
+  },
+  {
+    "revision": "937808d4189af01bd6f6",
+    "url": "/budgeteering/static/js/2.47a5ec02.chunk.js"
+  },
+  {
+    "revision": "a524c3e4483f4cd5a23e",
+    "url": "/budgeteering/static/js/main.03c3455d.chunk.js"
+  },
+  {
+    "revision": "d894856d1f65981191d7",
+    "url": "/budgeteering/static/js/runtime~main.b1c2f346.js"
+  }
+]);
