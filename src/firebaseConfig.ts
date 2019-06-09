0000245a9abe7c1cbdda0900683e89c4ed000962
adhe223 <adhe223@g.uky.edu>
2019-06-09 11:36:11 -0700

    Firebase api key doesn't need to be secret. Track it so I can use on github pages

---
+++ src/firebaseConfig.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyBZn-wkt-nRt9TwSZnvKd28jT4vy7ySgm4',
+  authDomain: 'budgeteering-70f5d.firebaseapp.com',
+  databaseURL: 'https://budgeteering-70f5d.firebaseio.com',
+  projectId: 'budgeteering-70f5d',
+  storageBucket: 'budgeteering-70f5d.appspot.com',
+  messagingSenderId: '473484708291',
+  appId: '1:473484708291:web:bf62d1245ff200c0',
+};
